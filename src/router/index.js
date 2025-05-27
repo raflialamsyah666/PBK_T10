ca5a524761a83de835161aca2888fe877139d506
raflialamsyah666 <raflialamsyah@student.uir.ac.id>
2025-05-27 07:26:09 +0700

    Menambahkan router

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Dashboard from '../views/Dashboard.vue'
+import Orders from '../views/Orders.vue'
+import Menu from '../views/Menu.vue'
+import Cart from '../views/Cart.vue'
+import History from '../views/History.vue'
+import Profile from '../views/Profile.vue'
+import Logout from '../views/Logout.vue'
+import Login from '../views/Login.vue' // jangan lupa tambahkan Login
+
+const routes = [
+  { path: '/', component: Dashboard },
+  { path: '/orders', component: Orders },
+  { path: '/menu', component: Menu },
+  { path: '/cart', component: Cart },
+  { path: '/history', component: History },
+  { path: '/profile', component: Profile },
+  { path: '/logout', component: Logout },
+  { path: '/login', component: Login },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
