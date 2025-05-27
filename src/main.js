020f55d16b4e7d0e041f50c92fbc757e4e85922d
raflialamsyah666 <raflialamsyah@student.uir.ac.id>
2025-05-27 07:31:13 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from '../../foodorder-app/src/router';
+import router from '../../food-order/src/router';
 import './assets/style.css';
 
 const app = createApp(App);
